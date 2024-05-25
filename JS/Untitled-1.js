34c875a8d0709baf2688c08e1ddd14b69ae09bad
Guilherme Monteiro <monteiro.guilherme.andrade@gmail.com>
2024-05-25 11:36:03 -0300

    updat0.0.1

--- JS/Untitled-1.js
+++ JS/Untitled-1.js
@@ -6,16 +6,35 @@ let divClasse = document.getElementById ('divClasse')
 
 
 let itens = document.getElementById('itens')
+let divItens = document.getElementById('divItens')
 
 let habilidades = document.getElementById('habilidades')
+
+let Olcultismo = document.getElementById('Olcultismo')
+let divOcultista = document.getElementById('divOcultista')
 
 
 raca.addEventListener('click', ()=> {
     divRaca.classList.add('divRacaTrue')
     divClasse.classList.remove('divClasseTrue')
+    divItens.classList.remove('divItensTrue')
 })
 
 classe.addEventListener('click', ()=> {
     divClasse.classList.add('divClasseTrue')
     divRaca.classList.remove('divRacaTrue')
+    divItens.classList.remove('divItensTrue')
+})
+
+itens.addEventListener('click', ()=> {
+    divItens.classList.add('divItensTrue')
+    divRaca.classList.remove('divRacaTrue')
+    divClasse.classList.remove('divClasseTrue')
+})
+
+Olcultismo.addEventListener('click', ()=>{
+    divOcultista.classList.add('divOlcultistaTrue')
+    divRaca.classList.remove('divRacaTrue')
+    divClasse.classList.remove('divClasseTrue')
+    divItens.classList.remove('divItensTrue')
 })
