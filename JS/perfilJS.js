c60f542fb684c9f510c5da08c4cb1ac1ea514f60
Guilherme Monteiro <monteiro.guilherme.andrade@gmail.com>
2024-07-25 10:45:11 -0300

    sla fdsssss

--- JS/perfilJS.js
+++ JS/perfilJS.js
@@ -1,19 +1,55 @@
-let imagemField = document.getElementById('imagemField')
-let imagemPreview= document.getElementById('imagemPreview')
+document.addEventListener('DOMContentLoaded', () => {
+    const imagemField = document.getElementById('imagemField');
+    const imagemPreview = document.getElementById('imagemPreview');
+    const imgPerfilElements = document.querySelectorAll('.imgPerfil');
+    const saveButton = document.getElementById('saveButton');
 
-let bannerField = document.getElementById('bannerField')
-let bannerPreview= document.getElementById('bannerPreview')
 
+    const usuario = document.getElementById('usuario')
+    const savedValue = localStorage.getItem("inputValue")
+    if (savedValue) {
+        usuario.value = savedValue
+    }
+    usuario.addEventListener("input", function() {
+        localStorage.setItem("inputValue", usuario.value)
+    })
 
-let loadImagem = (e) =>{
-    const filePath = e.target || window.event.srcElement;
+    const sobre = document.getElementById('sobre')
+    const savedValue2 = localStorage.getItem("textAreaValue")
+    if (savedValue2) {
+        sobre.value = savedValue2
+    }
+    sobre.addEventListener("input", function() {
+        localStorage.setItem("textAreaValue", sobre.value)
+    })
 
-    const file = filePath.files;
-    const fileRender = new FileReader();
-    fileRender.onload = () =>{
-        imagemPreview.src = fileRender.result;
+    const loadImagem = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const fileReader = new FileReader();
+            fileReader.onload = () => {
+                const dataUrl = fileReader.result;
+                imagemPreview.src = dataUrl;
+                imgPerfilElements.forEach(img => {
+                    img.src = dataUrl;
+                });
+            };
+            fileReader.readAsDataURL(file);
+        }
     };
-    fileRender.readAsDataURL(file[0]);
-}
 
-imagemField.addEventListener('change', loadImagem);
+    imagemField.addEventListener('change', loadImagem);
+
+    const savedImage = localStorage.getItem('editPerfil');
+    if (savedImage) {
+        imagemPreview.src = savedImage;
+        imgPerfilElements.forEach(img => {
+            img.src = savedImage;
+        });
+    }
+
+    saveButton.addEventListener('click', () => {
+        const contentPerfil = imagemPreview.src;
+        localStorage.setItem('editPerfil', contentPerfil);
+    });
+});
