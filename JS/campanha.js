e8dfa45f69f0df6e752263a777426eff38d54620
Guilherme Monteiro <monteiro.guilherme.andrade@gmail.com>
2024-07-26 11:37:46 -0300

    v 0.1.2

--- JS/campanha.js
+++ JS/campanha.js
@@ -2,15 +2,36 @@ document.getElementById("butao").addEventListener("click", function() {
     var gameCard = document.getElementById("gamecard");
     var button = document.getElementById("butao");
 
-   
+
     if (gameCard.style.display === "none") {
         gameCard.style.display = "block";
-        button.style.display = "none"; 
+        button.style.display = "none";
     } else {
         gameCard.style.display = "none";
-        button.style.display = "block"; 
+        button.style.display = "block";
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const SobreCampanha = document.getElementById('SobreCampanha')
+    const textSobreCampanha = localStorage.getItem('textSobreCampanha')
+    if (textSobreCampanha) {
+        SobreCampanha.value = textSobreCampanha
+    }
+    SobreCampanha.addEventListener("input", function() {
+        localStorage.setItem("textSobreCampanha", SobreCampanha.value)
+    })
+
+    const titleCampanha = document.getElementById('titleCampanha')
+    const savedTitle = localStorage.getItem('savedTitle')
+    if (savedTitle) {
+        titleCampanha.value = savedTitle
+    }
+    titleCampanha.addEventListener("input", function() {
+        localStorage.setItem("savedTitle", titleCampanha.value)
+    })
+})
+
 
 //--------------localStorage--------------//
 document.getElementById("imgPerfil").src = localStorage.getItem("editPerfil")
